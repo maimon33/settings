14f8fa2f6853c9286d44e2e5f9f4cbadbf8fcf49
Assi Maimon <maimon33@gmail.com>
2017-11-01 16:30:57 +0200

    add vimrc and vs_code settings change

---
+++ vimrc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+bash -c 'cat > ~/.vimrc <<EOL
+colorscheme delek
+syntax enable
+set tabstop=4
+set softtabstop=4
+set expandtab
+set number
+set showcmd
+set cursorline
+filetype indent on
+set wildmenu
+set lazyredraw
+set showmatch
+set incsearch
+set hlsearch
+set foldenable
+set foldlevelstart=10
+set foldnestmax=10
+set foldmethod=indent
+set backup
+set backupdir=~/.vim-tmp,~/.tmp,~/tmp,/var/tmp,/tmp
+set backupskip=/tmp/*,/private/tmp/*
+set directory=~/.vim-tmp,~/.tmp,~/tmp,/var/tmp,/tmp
+set writebackup
+EOL'
--- vs_code.sh
+++ vs_code.sh
@@ -2,8 +2,26 @@
 
 bash -c 'cat > ~/Library/Application\ Support/Code/User/settings.json <<EOL
 {
+    "python.linting.pylintEnabled": false,
     "files.autoSave": "onFocusChange",
+    "editor.rulers": [80,120],
     "vim.disableAnnoyingNeovimMessage": true,
-    "workbench.colorTheme": "Monokai Dimmed"
+    "workbench.colorTheme": "Monokai Dimmed",
+    "window.zoomLevel": 0,
+    "sync.gist": "294f60cdb8360e91af4422759c4cc3ba",
+    "sync.lastUpload": "2017-11-01T13:14:50.271Z",
+    "sync.autoDownload": false,
+    "sync.autoUpload": false,
+    "sync.lastDownload": "",
+    "sync.forceDownload": false,
+    "sync.anonymousGist": false,
+    "sync.host": "",
+    "sync.pathPrefix": "",
+    "sync.quietSync": false,
+    "sync.askGistName": false,
+    "cSpell.userWords": [
+        "isdigit"
+    ],
+    "cSpell.language": "en,en-US"
 }
 EOL'
