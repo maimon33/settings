c1cd17c554dcb58a35c6046ee5126dbd6d810ac2
Assi Maimon <assi.maimon@bluevine.com>
2017-11-12 10:43:42 +0200

    Add source folder (#2)
    * add source folder
    
    * edit profile
    
    * change script content
    
    * fix
    
    * fix

--- vimrc.sh
+++ vimrc.sh
@@ -3,7 +3,7 @@
 if [ -f ~/.vimrc ]; then
     echo "File found! Getting the latest from GitHub, Archving the old"
     mv ~/.vimrc ~/.vimrc.bak
-    curl -O https://raw.githubusercontent.com/maimon33/mac-setting/master/source/.vimrc ~/.vimrc
+    curl https://raw.githubusercontent.com/maimon33/mac-setting/master/source/.vimrc -o ~/.vimrc
 else
-    curl -O https://raw.githubusercontent.com/maimon33/mac-setting/master/source/.vimrc ~/.vimrc
+    curl https://raw.githubusercontent.com/maimon33/mac-setting/master/source/.vimrc -o ~/.vimrc
 fi
