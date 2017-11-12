e877fc9fea528c19bc65e5da5a04b1fd8813be03
Assi Maimon <maimon33@gmail.com>
2017-11-12 10:42:01 +0200

    fix

--- vs_code.sh
+++ vs_code.sh
@@ -3,7 +3,7 @@
 if [ -f ~/Library/Application\ Support/Code/User/settings.json ]; then
     echo "File found! Getting the latest from GitHub, Archving the old"
     mv ~/.vimrc ~/.vimrc.bak
-    curl -O https://raw.githubusercontent.com/maimon33/mac-setting/master/source/vs_code/settings.json ~/Library/Application\ Support/Code/User/settings.json
+    curl https://raw.githubusercontent.com/maimon33/mac-setting/master/source/vs_code/settings.json -o ~/Library/Application\ Support/Code/User/settings.json
 else
-    curl -O https://raw.githubusercontent.com/maimon33/mac-setting/master/source/vs_code/settings.json ~/Library/Application\ Support/Code/User/settings.json
+    curl https://raw.githubusercontent.com/maimon33/mac-setting/master/source/vs_code/settings.json -o ~/Library/Application\ Support/Code/User/settings.json
 fi
